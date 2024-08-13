df654aa5d8572242a48f6b1707b9ae17f39cd6b2
tsukumi <tsukumijima@users.noreply.github.com>
2024-08-14 01:44:00 +0900

    Update: JKCommentCrawler v1 同様のリトライ機能を v2 にも実装

--- jkcommentcrawler/__main__.py
+++ jkcommentcrawler/__main__.py
@@ -1,6 +1,7 @@
 
 import configparser
 import json
+import traceback
 import typer
 from datetime import datetime
 from ndgr_client import NDGRClient, XMLCompatibleComment
@@ -54,97 +55,130 @@ async def main(
     comment_counts: dict[str, int] = {}
     for jikkyo_channel_id in jikkyo_channel_ids:
 
-        # ダウンロードしたコメントを格納するリスト
-        comments: list[XMLCompatibleComment] = []
+        # 3回までリトライ
+        for retry_count in range(3):
+            try:
+                # ダウンロードしたコメントを格納するリスト
+                comments: list[XMLCompatibleComment] = []
 
-        # 指定された日付に一部でも放送された実況番組のコメントをすべてダウンロード
-        print(f'[{datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")}] Retrieve comments from {jikkyo_channel_id} broadcast during {target_date.strftime("%Y/%m/%d")}.')
-        nicolive_program_ids = await NDGRClient.getProgramIDsOnDate(jikkyo_channel_id, target_date)
-        print(f'Retrieving Nicolive comments from {len(nicolive_program_ids)} programs. ({", ".join(nicolive_program_ids)})')
-        print(Rule(characters='-', style=Style(color='#E33157')))
+                # 指定された日付に一部でも放送された実況番組のコメントをすべてダウンロード
+                print(f'[{datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")}]\\[{jikkyo_channel_id}] '
+                      f'Retrieve comments broadcast during {target_date.strftime("%Y/%m/%d")}.')
+                nicolive_program_ids = await NDGRClient.getProgramIDsOnDate(jikkyo_channel_id, target_date)
+                print(f'Retrieving Nicolive comments from {len(nicolive_program_ids)} programs.' +
+                      (f' ({", ".join(nicolive_program_ids)})' if len(nicolive_program_ids) > 1 else ''))
+                print(Rule(characters='-', style=Style(color='#E33157')))
 
-        # ニコニコ生放送番組 ID ごとに
-        for nicolive_program_id in nicolive_program_ids:
+                # ニコニコ生放送番組 ID ごとに
+                for nicolive_program_id in nicolive_program_ids:
 
-            # NDGRClient を初期化
-            ndgr_client = NDGRClient(nicolive_program_id, verbose=False, console_output=True)
+                    # NDGRClient を初期化
+                    ndgr_client = NDGRClient(nicolive_program_id, verbose=False, console_output=True)
 
-            # ニコニコアカウントにログイン (タイムシフト再生に必要)
-            ## すでにログイン済みの Cookie が cookies.json にあれば Cookie を再利用し、ない場合は新規ログインを行う
-            cookies_json = Path(__file__).parent.parent / 'cookies.json'
-            if cookies_json.exists():
-                with open(cookies_json, 'r', encoding='utf-8') as f:
-                    cookies_dict = json.load(f)
-                cookies_dict = await ndgr_client.login(cookies=cookies_dict)
-                # もし None が返る場合はログインセッションが切れた可能性が高いので、メールアドレスとパスワードを指定して再ログインを実行
-                if cookies_dict is None:
-                    cookies_dict = await ndgr_client.login(mail=niconico_mail, password=niconico_password)
-                    if cookies_dict is None:
-                        raise Exception('Failed to login to niconico.')
-                    with open(cookies_json, 'w', encoding='utf-8') as f:
-                        json.dump(cookies_dict, f)
-            else:
-                # cookies.json が存在しない場合は新規ログインを実行
-                cookies_dict = await ndgr_client.login(mail=niconico_mail, password=niconico_password)
-                if cookies_dict is None:
-                    raise Exception('Failed to login to niconico.')
-                with open(cookies_json, 'w', encoding='utf-8') as f:
-                    json.dump(cookies_dict, f)
+                    # ニコニコアカウントにログイン (タイムシフト再生に必要)
+                    ## すでにログイン済みの Cookie が cookies.json にあれば Cookie を再利用し、ない場合は新規ログインを行う
+                    cookies_json = Path(__file__).parent.parent / 'cookies.json'
+                    if cookies_json.exists():
+                        with open(cookies_json, 'r', encoding='utf-8') as f:
+                            cookies_dict = json.load(f)
+                        cookies_dict = await ndgr_client.login(cookies=cookies_dict)
+                        # もし None が返る場合はログインセッションが切れた可能性が高いので、メールアドレスとパスワードを指定して再ログインを実行
+                        if cookies_dict is None:
+                            cookies_dict = await ndgr_client.login(mail=niconico_mail, password=niconico_password)
+                            if cookies_dict is None:
+                                raise Exception('Failed to login to niconico.')
+                            with open(cookies_json, 'w', encoding='utf-8') as f:
+                                json.dump(cookies_dict, f)
+                    else:
+                        # cookies.json が存在しない場合は新規ログインを実行
+                        cookies_dict = await ndgr_client.login(mail=niconico_mail, password=niconico_password)
+                        if cookies_dict is None:
+                            raise Exception('Failed to login to niconico.')
+                        with open(cookies_json, 'w', encoding='utf-8') as f:
+                            json.dump(cookies_dict, f)
 
-            # コメントをダウンロードしてリストに追加
-            comments.extend([NDGRClient.convertToXMLCompatibleComment(comment) for comment in await ndgr_client.downloadBackwardComments()])
+                    # コメントをダウンロードしてリストに追加
+                    comments.extend([
+                        NDGRClient.convertToXMLCompatibleComment(comment)
+                        for comment in await ndgr_client.downloadBackwardComments()
+                    ])
 
-        # 指定された日付以外に投稿されたコメントを除外
-        print(f'Total comments for {jikkyo_channel_id}: {len(comments)}')
-        comments = [comment for comment in comments if datetime.fromtimestamp(comment.date_with_usec).date() == target_date]
-        print(f'Excluding comments posted on dates other than {target_date.strftime("%Y/%m/%d")} ...')
-        print(f'Final comments for {jikkyo_channel_id}: {len(comments)}')
-        comment_counts[jikkyo_channel_id] = len(comments)
+                # 指定された日付以外に投稿されたコメントを除外
+                print(f'Total comments for {jikkyo_channel_id}: {len(comments)}')
+                comments = [comment for comment in comments if datetime.fromtimestamp(comment.date_with_usec).date() == target_date]
+                print(f'Excluding comments posted on dates other than {target_date.strftime("%Y/%m/%d")} ...')
+                print(f'Final comments for {jikkyo_channel_id}: {len(comments)}')
+                comment_counts[jikkyo_channel_id] = len(comments)
 
-        # コメント投稿日時昇順で並び替え
-        ## ニコニコ実況と NX-Jikkyo のコメントを時系列でマージするためにこの処理が必要
-        comments.sort(key=lambda comment: comment.date_with_usec)
+                # コメント投稿日時昇順で並び替え
+                ## ニコニコ実況と NX-Jikkyo のコメントを時系列でマージするためにこの処理が必要
+                comments.sort(key=lambda comment: comment.date_with_usec)
 
-        # {kakolog_dir}/{jikkyo_channel_id}/{date.year}/{date.strftime('%Y%m%d')}.nicojk に保存
-        ## 取得できたコメントが一つもない場合は実行しない
-        if len(comments) > 0:
-            output_dir = kakolog_dir / jikkyo_channel_id / str(target_date.year)
-            output_dir.mkdir(parents=True, exist_ok=True)
-            output_file = output_dir / f'{target_date.strftime("%Y%m%d")}.nicojk'
-            # コメントリストを XML 文字列に変換
-            xml_content = NDGRClient.convertToXMLString(comments)
-            # 既存の XML ファイルがあれば文字数を取得
-            if output_file.exists():
-                with open(output_file, 'r', encoding='utf-8') as f:
-                    existing_length = len(f.read())
-            else:
-                existing_length = 0
-            # コメントデータを保存
-            if len(xml_content) == 0:
-                # コメントが1件も取得できていない場合は保存しない
-                print(f"Skipping log save for {target_date.strftime('%Y/%m/%d')} as there are 0 comments.")
-            elif existing_length > len(xml_content) and not force:
-                # 既存のファイルの方が文字数が多い場合は保存しない
-                print(f'Skipping log save as the previously retrieved log has more characters. (Previous: {existing_length} chars, Current: {len(xml_content)} chars)')
-            else:
-                # 既存のファイルの方が文字数が多いが、--force が指定されている場合は上書きする
-                if existing_length > len(xml_content) and force:
-                    print(f'The previously retrieved log has more characters, but overwriting as --force is specified. (Previous: {existing_length} chars, Current: {len(xml_content)} chars)')
-                # ファイルに書き込む
-                with open(output_file, 'w', encoding='utf-8') as f:
-                    f.write(xml_content)
-                print(f"Log saved to {output_file}.")
-        elif len(comments) == 0:
-            print(f'No comments found for {jikkyo_channel_id} on {target_date.strftime("%Y/%m/%d")}. Skipping...')
-        print(Rule(characters='=', style=Style(color='#E33157')))
+                # {kakolog_dir}/{jikkyo_channel_id}/{date.year}/{date.strftime('%Y%m%d')}.nicojk に保存
+                ## 取得できたコメントが1つもない場合は実行しない
+                if len(comments) > 0:
+                    output_dir = kakolog_dir / jikkyo_channel_id / str(target_date.year)
+                    output_dir.mkdir(parents=True, exist_ok=True)
+                    output_file = output_dir / f'{target_date.strftime("%Y%m%d")}.nicojk'
 
+                    # コメントリストを XML 文字列に変換
+                    xml_content = NDGRClient.convertToXMLString(comments)
+
+                    # 既存の XML ファイルがあれば文字数を取得
+                    if output_file.exists():
+                        with open(output_file, 'r', encoding='utf-8') as f:
+                            existing_length = len(f.read())
+                    else:
+                        existing_length = 0
+
+                    # コメントが1件も取得できていない場合は過去ログを保存しない
+                    if len(xml_content) == 0:
+                        print(f"Skipping log save for {target_date.strftime('%Y/%m/%d')} as there are 0 comments.")
+
+                    # 既存のファイルの方が文字数が多い場合は過去ログを保存しない
+                    elif existing_length > len(xml_content) and not force:
+                        print(f'Skipping log save as the previously retrieved log has more characters. '
+                              f'(Previous: {existing_length} chars, Current: {len(xml_content)} chars)')
+
+                    # 過去ログを保存
+                    else:
+                        # 既存のファイルの方が文字数が多いが、--force が指定されている場合は上書きする
+                        if existing_length > len(xml_content) and force:
+                            print(f'The previously retrieved log has more characters, but overwriting as --force is specified. '
+                                  f'(Previous: {existing_length} chars, Current: {len(xml_content)} chars)')
+                        # ファイルに書き込む
+                        with open(output_file, 'w', encoding='utf-8') as f:
+                            f.write(xml_content)
+                        print(f"Log saved to {output_file}.")
+
+                # コメントが1件も取得できていない場合はスキップ
+                elif len(comments) == 0:
+                    print(f'No comments found for {jikkyo_channel_id} on {target_date.strftime("%Y/%m/%d")}. Skipping ...')
+                print(Rule(characters='=', style=Style(color='#E33157')))
+
+                # 正常にダウンロードできたらループを抜ける
+                break
+
+            except Exception:
+                if retry_count < 3:
+                    # エラー発生時は3回までリトライ
+                    print(f'[{datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")}]\\[{jikkyo_channel_id}] '
+                          f'Unexpected error occurred. Retrying ({retry_count + 1}/3) ...')
+                    print(traceback.format_exc())
+                else:
+                    # リトライ失敗、このチャンネルはスキップして次の実況チャンネルへ
+                    print(f'[{datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")}]\\[{jikkyo_channel_id}] '
+                          f'Unexpected error occurred. Retrying failed. Skipping ...')
+                    print(traceback.format_exc())
+
+    # 全チャンネルをダウンロードしたときは、各チャンネルごとの合計コメント数を表示
     if channel_id == 'all':
         print('Download completed for all channels.')
         for jikkyo_channel_id, count in comment_counts.items():
             print(f'{jikkyo_channel_id:>5}: {count:>5} comments')
         print(Rule(characters='=', style=Style(color='#E33157')))
 
-    # --save-dataset-structure-json が指定されている場合
+    # --save-dataset-structure-json が指定されているときは、データセットの構造を JSON ファイルに保存
     if save_dataset_structure_json is True:
         def get_directory_contents(directory_path: Path, nest: bool = False) -> dict[str, dict[str, dict[str, None]]]:
             if not directory_path.exists():
